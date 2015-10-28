0e4e779fa9b9975a5e86054bdaa8833d8a3d9084
youzipi <blake_12@qq.com>
2015-10-28 16:47:01 +0800

    update decorator.py

--- function/decorator.py
+++ function/decorator.py
@@ -2,22 +2,106 @@
 # user: youzipi
 # date: 15-10-28 下午1:05
 import datetime
+import functools
+import time
+import timeit
 
 current = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+DEBUG = 'debug'
 
 
-def log(func):
+def log0(func):
     def wrapper(*args, **kwargs):
         print current, ": call %s()" % func.__name__
         print (args, kwargs)
+        return func(*args, **kwargs)
+        # func(*args, **kwargs)
+
+    return wrapper
+
+
+def log1(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print current, "call %s()" % func.__name__
         return func(*args, **kwargs)
 
     return wrapper
 
 
-@log
+def log2(suffix='info'):
+    def decorator(func):  # equal to log1
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            print current, "[%s]: call %s()" % (suffix, func.__name__)
+            return func(*args, **kwargs)  # execute target function
+
+        return wrapper
+
+    return decorator
+
+
+# Flask-like class-based decorator
+class Log:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def debug(cls):
+        def decorator(func):  # equal to log1
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                print current, "[%s]: call %s()" % ("debug", func.__name__)
+                return func(*args, **kwargs)  # execute target function
+
+            return wrapper
+
+        return decorator
+
+
+def timed(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print "timed"
+        start = time.clock()
+        func(*args, **kwargs)
+        end = time.clock()
+        print 'used:', end - start
+
+    return wrapper
+
+
+log = Log()
+
+
+@log.debug()
 def add(a, b):
     print a + b
 
 
-add(1, 2)
+@log2('debug')
+def add2(a, b):
+    print a + b
+
+
+@log1
+@timed
+def multiply(numbers):
+    """
+    Multiple decorators
+    equals to log1( timed( multiply(numbers) ) )
+    output>>
+        # the outer function execute first
+        2015-10-28 16:38:00 call multiply() #log1
+        timed   #timed
+        120
+        used: 2.4e-05   #timed
+    """
+    print reduce(lambda a, b: a * b, numbers)
+
+
+# add(1, 2)
+
+# add2(1, 2)
+
+multiply([1, 2, 3, 4, 5])
